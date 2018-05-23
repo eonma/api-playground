163066773d1b469aba756e7038b2b10e637d1eb4
Ma, Chen(chenma) <chenma@paypal.com>
2018-05-23 09:02:01 +0100

    Change-Id: Iaa727aee2f58f52af479da119ef6b744b339955b

--- api-playground/src/TestServlet.java
+++ api-playground/src/TestServlet.java
@@ -27,7 +27,7 @@ public class TestServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("/jsp/product/mp/orderv2.jsp?").forward(request, response);
 	}
 
 	/**
